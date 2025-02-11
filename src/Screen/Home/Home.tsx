142fbe88eb74b337eab6e591068d1b5e69e2e58c
bexruz-cyber <mayoq2024@gmail.com>
2025-02-11 16:32:32 +0500

    add

--- src/Screen/Home/Home.tsx
+++ src/Screen/Home/Home.tsx
@@ -7,9 +7,14 @@ const Home = () => {
         const tg = window.Telegram?.WebApp;
         if (tg) {
             tg.ready();
+            console.log("Telegram WebApp object:", tg);
+            console.log("initDataUnsafe:", tg.initDataUnsafe);
             setUser(tg.initDataUnsafe?.user || null);
+        } else {
+            console.log("Telegram WebApp not found.");
         }
     }, []);
+
 
     return (
         <div>
