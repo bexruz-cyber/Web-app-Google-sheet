ce893064ae4ce6cffb67bdfbd7b4c5631378b4ac
bexruz-cyber <mayoq2024@gmail.com>
2025-02-11 14:51:59 +0500

    add

--- src/Router/Navigation.tsx
+++ src/Router/Navigation.tsx
@@ -1,19 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Sales from "../Screen/Sales/Sales"
-import Login from "../Screen/Auth/Login"
-import Start from "../Screen/Start/Start"
 import Production from "../Screen/Production/Production"
 import SupplyDepartment from "../Screen/Supply department/SupplyDepartment"
 import MarketingDepartment from "../Screen/Marketing department/MarketingDepartment"
 import Dashboard from "../Screen/Dashhboard/Dashboard"
-import AddWorker from "../Screen/AddWorker/AddWorker"
 
 const Navigation = () => {
     return (
         <Routes>
-            <Route path="/" element={<Start />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/add_worker" element={<AddWorker />} />
             <Route path="/sales" element={<Sales />} />
             <Route path="/production" element={<Production />} />
             <Route path="/suplyDepartment" element={<SupplyDepartment />} />
