3fea54e9a728450c62a52e8d2a5702c8b433024b
bexruz-cyber <mayoq2024@gmail.com>
2025-02-11 15:10:44 +0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  
-  
 
   return (
-    <div>
+    <>
       <Navigation />
       <ToastContainer position="top-right" autoClose={3000} />
-    </div>
+    </>
   );
 };
 
