c1aaee1e121be7ac2a273c15e5dfa4e96f9ece0a
bexruz-cyber <mayoq2024@gmail.com>
2025-02-12 16:50:57 +0500

    add

--- src/Screen/Home/Home.tsx
+++ src/Screen/Home/Home.tsx
@@ -1,19 +1,6 @@
-import { useState, useEffect } from "react";
-
 const Home: React.FC = () => {
-    const [userId, setUserId] = useState<number | null>(null);
-
-    useEffect(() => {
-        if (window.Telegram?.WebApp?.initDataUnsafe?.user) {
-            setUserId(window.Telegram.WebApp.initDataUnsafe.user.id);
-        } else {
-            console.warn("Foydalanuvchi ma'lumoti mavjud emas");
-        }
-    }, []);
-
     return (
         <div>
-            <h1>ID: {userId !== null ? userId : "Yuklanmoqda..."}</h1>
         </div>
     );
 };
